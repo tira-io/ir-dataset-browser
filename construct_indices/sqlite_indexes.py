8120b93315bf8cf6815aee6aa1480b28b300415b
Maik Fröbe <maik.froebe@uni-jena.de>
2024-01-02 21:03:56 +0100

    improve db structure

--- construct_indices/sqlite_indexes.py
+++ construct_indices/sqlite_indexes.py
@@ -13,6 +13,17 @@ def create_db(filename):
 
     con.commit()
     cur.close()
+    con.close()
+
+    con = sqlite3.connect(filename.replace('.json.gz', '.db'))
+    cur = con.cursor()
+    # From https://github.com/phiresky/sql.js-httpvfs
+    cur.execute("pragma journal_mode = delete;")
+    cur.execute("pragma page_size = 1024;")
+    cur.execute("vacuum;")
+    con.commit()
+    cur.close()
+    con.close()
 
 if __name__ == '__main__':
     create_db('static/indexes/ms-marco.json.gz')
